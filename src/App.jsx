225b7b853d717e38378d499bcfa81a9d15682217
ianlee08 <ileejam123@gmail.com>
2024-08-11 17:51:19 +0900

    fix: fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Goblin Twn</h1>
+      <h1>Goblin Town</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count - 1)}>
           count is {count}
